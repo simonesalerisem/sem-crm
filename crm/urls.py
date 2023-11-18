96f50135c132fce56330d4ddad4582cf4a94ea1a
Simone Saleri <132743672+simonesalerisem@users.noreply.github.com>
2023-11-18 08:45:26 +0100

    init commit

--- crm/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from management_app.views import some_view
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('pdf/<int:order_id>/', some_view, name='pdf_view'),
-]
