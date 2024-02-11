7be5e5a1f2a4181cc87febaed50bc6c5e6f0d900
Simone Saleri <132743672+simonesalerisem@users.noreply.github.com>
2024-02-11 08:49:42 +0100

    init

---
+++ materialize-admin-v12.4.0/react-version/javascript-version/full-version/src/_codux/boards/home.board.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { createBoard } from '@wixc3/react-board';
+import _app from '../../pages/_app';
+
+export default createBoard({
+    name: 'home',
+    Board: () => null,
+    isSnippet: true,
+});
